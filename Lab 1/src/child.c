1ee918fb09c5b8038cb29d97919673fbb04e7d11
alshved <alshvedu@yandex.ru>
2024-10-16 20:12:41 +0300

    Fixed2

--- Lab 1/src/child.c
+++ Lab 1/src/child.c
@@ -1,56 +1,125 @@
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <ctype.h>
 
-#define BUFFER_SIZE 2024
+#define BUFFER_SIZE 2048
+#define MAX_NUMBERS 100
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        exit(1);
+void convertStringToIntArray(const char *str, int intArray[], int *size)
+{
+    int i = 0, num = 0;
+    *size = 0;
+    while (str[i] != '\n')
+    {
+        while (str[i] != '\n' && isspace(str[i]))
+        {
+            i++;
+        }
+        if (str[i] == '\n')
+        {
+            break;
+        }
+        num = 0;
+        while (str[i] != '\n' && isdigit(str[i]))
+        {
+            num = num * 10 + (str[i] - '0');
+            i++;
+        }
+        if (*size < MAX_NUMBERS)
+        {
+            intArray[*size] = num;
+            (*size)++;
+        }
+    }
+} // Перевод строки в инт
+
+int main(int argc, char *argv[])
+{
+
+    pid_t pid = getpid();
+    if (argc != 2)
+    {
+        const char msg[] = "Usage: <program> <filename>\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        exit(EXIT_FAILURE);
     }
 
     char *filename = argv[1];
-    FILE *file = fopen(filename, "w");
-    if (!file) {
-        perror("fopen");
+
+    int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (file == -1)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    } // Открываем файл
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    {
+        char msg[128];
+        int32_t len =
+            snprintf(msg, sizeof(msg) - 1,
+                     "%d: Start typing lines of text. Press 'Ctrl-D' or 'Enter' with no input to exit\n", pid);
+        write(STDOUT_FILENO, msg, len);
+    }
+
+    while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
+    {
+
+        if (buffer[0] == '\n')
+        {
+            break;
+        }
+        int array[MAX_NUMBERS];
+        int size = 0;
+        convertStringToIntArray(buffer, array, &size);
+        if (size < 2)
+        {
+            const char msg[] = "You have written few numbers\n";
+            int32_t written = write(STDOUT_FILENO, msg, sizeof(msg));
+            if (written != sizeof(msg))
+            {
+                const char msg[] = "error: failed to write to file\n";
+                write(STDERR_FILENO, msg, sizeof(msg));
+                exit(EXIT_FAILURE);
+            }
+        } // Обработка малого количества аргументов
+        int divisible = array[0];
+        for (int i = 1; i != size; ++i)
+        {
+            if (array[i] == 0)
+            {
+                const char ermsg[] = "Division by zero\n";
+                write(STDERR_FILENO, ermsg, sizeof(ermsg));
+                exit(EXIT_FAILURE);
+            }
+            char msg[32];
+            int32_t len = snprintf(msg, sizeof(msg) - 1, "%d : %d = %lf\n", divisible, array[i],
+                                   ((float)divisible / array[i]));
+            int32_t written = write(file, msg, len);
+            if (written != len)
+            {
+                const char msg[] = "error: failed to write to file\n";
+                write(STDERR_FILENO, msg, sizeof(msg));
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    if (bytes_read == -1)
+    {
+        const char msg[] = "error: failed to read from stdin\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        close(file);
         exit(1);
     }
 
-    char buffer[BUFFER_SIZE];
-    while (fgets(buffer, BUFFER_SIZE, stdin)) {
-        char *token = strtok(buffer, " ");
-        if (token == NULL) {
-            continue;
-        }
-
-        double first_num = atoi(token);
-        int division_by_zero = 0;
-        char result[BUFFER_SIZE];
-        snprintf(result, BUFFER_SIZE, "%f", first_num); // Записываем первое число в результат
-
-        token = strtok(NULL, " ");
-        while (token != NULL) {
-            int num = atoi(token);
-            if (num == 0) {
-                division_by_zero = 1;
-                break;
-            }
-            first_num /= num;
-            snprintf(result + strlen(result), BUFFER_SIZE - strlen(result), " / %d", num);
-            token = strtok(NULL, " ");
-        }
-
-        if (division_by_zero) {
-            fprintf(stderr, "Error: Division by zero detected. Terminating processes.\n");
-            fclose(file);
-            exit(1); // Завершаем дочерний процесс
-        }
-
-        fprintf(file, "Result: %f\n", first_num);
-    }
-
-    fclose(file);
+    close(file);
     return 0;
 }
--- Lab 1/src/parent.c
+++ Lab 1/src/parent.c
@@ -13,33 +13,71 @@ int main()
     pid_t pid;
     char buffer[BUFFER_SIZE];
     char filename[100];
+    ssize_t bytes_read, bytes_written;
 
     // Создание pipe1
     if (pipe(pipe1) == -1)
     {
         perror("pipe");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    printf("Введите имя файла: ");
-    scanf("%s", filename);
+
+    // Сообщение пользователю для ввода имени файла
+    const char *msg = "Введите имя файла: ";
+    bytes_written = write(STDOUT_FILENO, msg, strlen(msg));
+    if (bytes_written == -1)
+    {
+        const char error_msg[] = "error: failed to write to stdout\n";
+        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+        exit(EXIT_FAILURE);
+    }
+
+    // Чтение имени файла с консоли
+    bytes_read = read(STDIN_FILENO, filename, sizeof(filename) - 1);
+    if (bytes_read == -1)
+    {
+        const char error_msg[] = "error: failed to read from stdin\n";
+        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+        exit(EXIT_FAILURE);
+    }
+
+    // Удаление символа новой строки, если он был введен
+    if (bytes_read > 0 && filename[bytes_read - 1] == '\n')
+    {
+        filename[bytes_read - 1] = '\0';
+    }
+    else
+    {
+        filename[bytes_read] = '\0';
+    }
 
     // Создание дочернего процесса
-    pid = fork();
+    const pid_t child = fork();
 
-    if (pid == -1)
+    if (child == -1)
     {
         perror("fork");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    if (pid == 0)
+    if (child == 0)
     {
         // Дочерний процесс
-        // Закрытие ненужных сторон pipe'ов
+        pid_t pid = getpid();
+        {
+            char msg[64];
+            const int32_t length = snprintf(msg, sizeof(msg), "%d: I'm a child\n", pid);
+            write(STDOUT_FILENO, msg, length);
+        }
+
         close(pipe1[1]);
 
         // Перенаправление стандартного ввода на pipe1
-        dup2(pipe1[0], STDIN_FILENO);
+        if (dup2(pipe1[0], STDIN_FILENO) == -1)
+        {
+            perror("dup2");
+            exit(1);
+        }
         close(pipe1[0]);
 
         // Запуск дочерней программы
@@ -50,25 +88,42 @@ int main()
     else
     {
         // Родительский процесс
-        // Закрытие ненужных сторон pipe'ов
+        // Закрытие ненужной стороны pipe'а
         close(pipe1[0]);
-        ssize_t bytes;
-        while (bytes = read(STDIN_FILENO, buffer, sizeof(buffer)))
-        {
 
-            if (bytes < 0)
-            {
-                const char msg[] = "error: failed to read from stdin\n";
-                write(STDERR_FILENO, msg, sizeof(msg));
-                exit(EXIT_FAILURE);
-            }
-            else if (buffer[0] == '\n')
+        pid_t pid = getpid(); 
+			{
+				char msg[64];
+				const int32_t length =
+				    snprintf(msg, sizeof(msg), "%d: I'm a parent, my child has PID %d\n", pid, child);
+				write(STDOUT_FILENO, msg, length);
+			}
+
+        // Чтение данных с консоли и запись в pipe
+        while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
+        {
+            if (buffer[0] == '\n')
             {
                 break;
             }
-            write(pipe1[1], buffer, strlen(buffer));
+
+            bytes_written = write(pipe1[1], buffer, bytes_read);
+            if (bytes_written == -1)
+            {
+                const char error_msg[] = "error: failed to write to pipe\n";
+                write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+                exit(EXIT_FAILURE);
+            }
         }
 
+        if (bytes_read == -1)
+        {
+            const char error_msg[] = "error: failed to read from stdin\n";
+            write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+            exit(EXIT_FAILURE);
+        }
+
+        // Закрытие pipe'а после записи
         close(pipe1[1]);
 
         // Ожидание завершения дочернего процесса
